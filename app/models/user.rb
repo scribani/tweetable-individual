efde5b7b27bf3c333d533806c077ae97267c6ab0
Scribani <scribani.andres@gmail.com>
2021-08-27 16:11:41 +0000

    Add Tweet model, controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :tweets, dependent: :nullify
+
   # Validations
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: :show
+  resources :tweets, except: :new
 end
