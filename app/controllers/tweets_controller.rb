53e4db5c168373541287d0af93a8e8622ba363a6
Scribani <scribani.andres@gmail.com>
2021-08-27 19:57:40 +0000

    Add actions in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,44 @@
 class TweetsController < ApplicationController
-  def index; end
+  def index
+    @tweets = Tweet.all
+  end
 
-  def show; end
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments
+  end
 
-  def create; end
+  def create
+    @tweet = Tweet.new do |t|
+      t.body = params[:body]
+      t.user = current_user
+    end
 
-  def edit; end
+    if @tweet.save
+      redirect_to @tweet
+    else
+      redirect_back fallback_location: root_path
+    end
+  end
 
-  def update; end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
 
-  def destroy; end
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+      redirect_to @tweet
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+
+    redirect_back fallback_location: root_path
+  end
 end
